5943b7671083cf0dc16dc25cb9e4ae41abc8a7e6
Rob Weddell <rweddell4@gmail.com>
2022-04-02 12:19:43 -0500

    Create assessment1.py

---
+++ Assessments/assessment1.py
@@ -0,0 +1,72 @@
+'''
+Work through these exercises. Run them one-by-one as you complete them. 
+Once you are done with an exercise, surround it with comments so that the output is easier to read each time you run the file
+'''
+
+'''
+Exercise 1:
+- Write a code block that will print 'Hello World!'
+'''
+
+
+'''
+Exercise 2:
+- Write a code block that ask the user for their name and prints it back to screen
+'''
+
+
+
+'''
+Exercise 3:
+- Write a function that asks the user for their name and prints it back to screen prepended with 'Hello, ' and ends with '!'
+- Example: 'Hello, Rob!'
+'''
+
+
+
+
+'''
+Exercise 4:
+- Write a function that will compute the square of a number
+- The function should take no arguments
+- The function should ask the user for a number
+- Print the result to screen
+'''
+
+
+
+
+
+'''
+Exercise 5:
+- Write a function that will take two arguments: an integer called num, and a list of integers called numbers
+- The function checks that if num is in numbers
+- If num is in numbers, return True.
+- Otherwise return False
+'''
+
+
+
+
+
+'''
+Exercise 6:
+- Write a function called is_vowel that takes one argument: a string called char
+- If char has a length greater than 1, return False
+- If char is a vowel, return True
+- Otherwise return False
+'''
+
+
+
+
+
+'''
+Exercise 7:
+- Write a function called count_vowels that takes one argument: a string called phrase
+- Count the number of vowels in phrase and return the count
+'''
+
+
+
+
